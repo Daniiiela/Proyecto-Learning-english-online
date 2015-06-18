2cd5b5664b3527a366b8a70d625376cfae07389a
Daniiiela <adnfda@hotmail.com>
2015-06-18 17:37:22 -0500

    Pantalla de inicio mejorada

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$820977bf04b6f918$8c8d97265406c471bce4a7cebf87faa719f46cf2"
+password="pbkdf2(1000,20,sha512)$98b4f7d221805aea$9a26abacc0a5db4abdc56701d1a0e764d11d3a83"
